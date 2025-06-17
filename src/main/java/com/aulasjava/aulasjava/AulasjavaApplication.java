5afd27e9cfa2e663a6d36fe67f74642296c29018
rafacostadev <rafaelcostamantis@gmail.com>
2025-06-16 22:42:14 -0300

    inicial

---
+++ src/main/java/com/aulasjava/aulasjava/AulasjavaApplication.java
@@ -0,0 +1,19 @@
+package com.aulasjava.aulasjava;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasjavaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasjavaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+}
